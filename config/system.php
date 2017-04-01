e2956f4709ce578a79a072142e386232e1255fb0
Wilson Pilco Nuñez <wilsonaux1@gmail.com>
2017-04-01 01:26:22 -0500

    config

--- config/system.php
+++ config/system.php
@@ -1,15 +1,16 @@
 <?php
 return [
     'OS' => 'windows',
-    'OSs' => [
-        'windows',
-        'ubuntu'
-    ],
 
     /* 
         Necesario en windows
         Ubicacion de pdftotext binaries, en ubuntu no sera necesario mas que copiar la clave, 
         porque los comandos disponene de los binarios desde cualquier ubicacion
     */
-    'pdftotext' => 'C:/xpdfbin-win-3.04/bin64/pdftotext'
+
+    // Windows
+    'pdftotext' => 'C:/xpdfbin-win-3.04/bin64/pdftotext',
+
+    // Linux
+    // 'pdftotext' => 'pdftotext',
 ];
